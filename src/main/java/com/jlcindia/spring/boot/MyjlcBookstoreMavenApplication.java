620c9260e9c1a62f74ff3900251b7012ff57e871
my global <my global@mail.com>
2022-05-28 22:51:03 +0530

    first commit of jlc bookstore application

---
+++ src/main/java/com/jlcindia/spring/boot/MyjlcBookstoreMavenApplication.java
@@ -0,0 +1,13 @@
+package com.jlcindia.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjlcBookstoreMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjlcBookstoreMavenApplication.class, args);
+	}
+
+}
